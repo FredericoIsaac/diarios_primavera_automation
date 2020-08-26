2ce8e133fc632eaf99a1dc2c3113fa278d859e6f
Frederico.Gago <fredyisaac@gmail.com>
2020-08-26 20:04:43 +0100

    primeiro trial impressao

--- iniciar_primavera.py
+++ iniciar_primavera.py
@@ -43,16 +43,81 @@ def entrar_empresa(empresa):
     pyautogui.press('enter')
 
 
-    
+def tirar_diarios(diarios, data, empresa):
+    """
+    Tirar diarios da empresa durante o ano todo
+    """
+    # Extrato diarios
+    pyautogui.click('imagens/extrato_diarios.png')
+    pausa_tempo(2)
 
-# empresa = input("Insira a empresa desejada: ")
-# entrar_primavera()
-# pausa_tempo(15)
-# entrar_empresa(empresa)
-# pausa_tempo(20)
+    # Colocar data incial
+    data_inicial_imagem = pyautogui.locateOnScreen('imagens/data_inicial.png')
+    center_data_inicial = pyautogui.center(data_inicial_imagem)
+
+    # Colocar data fim
+    data_final_imagem = pyautogui.locateOnScreen("imagens/data_final.png")
+    center_data_final = pyautogui.center(data_final_imagem)
+
+    # Coloca o numero do diario           
+    diarios_imagem = pyautogui.locateOnScreen('imagens/introducao_diarios.png')
+    center_diarios_imagem = pyautogui.center(diarios_imagem)
+    
+    for diario in diarios:
+        # Escrever o diario
+        pyautogui.click(center_diarios_imagem.x + 188, center_diarios_imagem.y, clicks=2)
+        pyautogui.write(diario)
+
+        for mes, mes_dia in data.items():
+            pyautogui.click(center_data_inicial.x + 44, center_data_inicial.y)
+            # Escreve inicio do mes corrente
+            pyautogui.write(mes_dia[0])
+
+            pyautogui.click(center_data_final.x + 40, center_data_final.y)
+            # Escreve final do mes corrente
+            pyautogui.write(mes_dia[1])
+
+            # Atualiza o diario
+            pyautogui.click("imagens/atualizar_icnone.png")
+            pausa_tempo(10)
+
+            # Caso nao haja lançamentos no mês            
+            if pyautogui.locateOnScreen('imagens/organizar_numero_doc.png') == None:
+                continue
+
+            elif diario in ("20","30"):
+                # Organizar Lançamentos
+                pyautogui.click("imagens/organizar_numero_doc.png")
+                localizar_rato = pyautogui.position()
+                pyautogui.click(localizar_rato[0],localizar_rato[1] + 40, button="right")
+                pausa_tempo(0.5)
+                pyautogui.click("imagens/organizar_ascendente.png")
+                pausa_tempo(5)
+
+            # Imprimir
+            pyautogui.click("imagens/imprimir_icone.png")
+            pausa_tempo(0.5)
+            pyautogui.press("enter")
+            pausa_tempo(10)
+            pyautogui.click("imagens/print_pdf.png")
+            pausa_tempo(2)
+            pyautogui.write("{} - Extrato diario {} de {}-2019".format(empresa, diario, mes_dia[0][2:4]))
+            pyautogui.press("enter")
+            pausa_tempo(1)
+
+            # Sair da janela de impressao
+            extrato_impressao = pyautogui.locateOnScreen('imagens/exit_impressao_I.png')
+            center_extrato_impressao = pyautogui.center(extrato_impressao)
+            pyautogui.click(center_extrato_impressao.x + 733, center_extrato_impressao.y)
+
+
+            
+
+            
+empresas_diarios = ["20225","10162","10119"]
 
 # List com o numero de diarios que se vai tirar
-numero_diarios = [10,20,30,40,60,61,62,63,71]
+numero_diarios = ["20","30","40","60","61","62","63","71"]
 
 # Dict com as datas do inicio do mes e fim de 2019
 data = {
@@ -71,34 +136,13 @@ data = {
 }
 
 
+# entrar_primavera()
+# pausa_tempo(15)
 
-def tirar_diarios(diarios, data):
-    """
-    Tirar diarios da empresa durante o ano todo
-    """
-    # Extrato diarios
-    pyautogui.click('imagens/extrato_diarios.png')
-    pausa_tempo(2)
-
-    # Colocar data incial
-    data_inicial_imagem = pyautogui.locateOnScreen('imagens/data_inicial.png')
-    center_data_inicial = pyautogui.center(data_inicial_imagem)
-
-    # Colocar data fim
-    data_final_imagem = pyautogui.locateOnScreen("imagens/data_final.png")
-    center_data_final = pyautogui.center(data_final_imagem)
-    
-    for diario in numero_diarios:
-        for mes, mes_dia in data.items():
-            pyautogui.click(center_data_inicial.x + 44, center_data_inicial.y)
-            # Escreve inicio do mes corrente
-            pyautogui.write(mes_dia[0])
-            pyautogui.click(center_data_final.x + 40, center_data_final.y)
-            # Escreve final do mes corrente
-            pyautogui.write(mes_dia[1])
-
-    
+for i in range(len(empresas_diarios)):
+    entrar_empresa(empresa[i])
+    pausa_tempo(15)
+    tirar_diarios(numero_diarios, data, empresas_diarios[i])
+        
 
 
-
-tirar_diarios(numero_diarios, data)

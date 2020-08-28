3bc33fe789aef236b8ef9810043fe9a1b25566e9
Frederico.Gago <fredyisaac@gmail.com>
2020-08-28 17:51:05 +0100

    atualização inputs pdf_print

--- pdf_print_last.py
+++ pdf_print_last.py
@@ -1,15 +1,27 @@
 # importing required modules 
 import PyPDF2 
 
-#meses = ["03","06","09","12"]
-meses = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-#diario = ["20","30","60","61","62","71"]
-#diario = ["61","71"]
-diario = ["60"]
-empresa = "20207"
+# Pede a empresa a que corresponde
+empresa = input("Empresa: ")
 
+# Pede os diarios a tirar
+diarios = []
+diario_input = input("Insira os diarios que pretende tirar: ")
+while diario_input:
+    diarios.append(diario_input)
+    diario_input = input("Insira os diarios que pretende tirar: ")
 
-for diario in diario:
+# Pede os meses a tirar
+meses = []
+mes_input = input("Insira os meses que pretende tirar (enter para o ano inteiro): ")
+if not mes:
+    meses = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+else:
+    while mes_input:     
+        meses.append(mes_input)
+        mes_input = input("Insira os meses que pretende tirar: ")
+
+for diario in diarios:
     for mes in meses:
         # creating a pdf file object 
         pdfFileObj = open('pdf/{} - Extrato diario {} de {}-2019.pdf'.format(empresa, diario, mes), 'rb') 
@@ -37,7 +49,4 @@ for diario in diario:
         # closing the new pdf file object 
         newFile.close()
     
-
-
-
 
